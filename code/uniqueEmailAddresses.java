68c7218584a12c50e05691078e8c7f11ded85ff6
Hilal Tarakci <hlltarakci@gmail.com>
2019-11-10 08:58:10 +0300

    add time and space complexity explanation

--- code/uniqueEmailAddresses.java
+++ code/uniqueEmailAddresses.java
@@ -4,6 +4,11 @@ class Solution {
     
     time O(total content of emails) - a.k.a O(numOfEmails * string size) - string concat takes time proportional to its size
     space O(total content of emails) - worst case: every email is different
+    
+    another interpretation on time and space:
+    time O(n) - where n is num of emails
+    space O(n)
+    with assumption - size of email address is bound, so any operations on them can be considered O(1) -- emails are short strings
       
     */
     public int numUniqueEmails(String[] emails) {

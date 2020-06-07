e81123b0d182f4fe28e6284393466d63d2576270
Hilal Tarakci <hlltarakci@gmail.com>
2020-06-07 20:05:26 +0300

    Create validateBinarySearchTree.java

---
+++ code/f/extra/validateBinarySearchTree.java
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/validate-binary-search-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /*
+        n: num of nodes
+        h: tree height --- log n for balanced, n for unbalanced
+        time: O(n)
+        space: O(h)
+    */
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
+    }
+    
+    private boolean isValidBST(TreeNode root, Integer min, Integer max) {
+        if(root == null) return true;
+        if( min != null && root.val <= min || 
+            max != null && root.val >= max) return false;
+        
+        return isValidBST(root.left, min, root.val) && 
+            isValidBST(root.right, root.val, max);
+    }
+}

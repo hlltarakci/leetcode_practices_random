4b0feebd20f2b388a6a19e038e3ec66fd028ea2a
Hilal Tarakci <hlltarakci@gmail.com>
2020-06-07 19:45:59 +0300

    Create lowestCommonAncestorOfABinaryTree.java

---
+++ code/f/extra/lowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    /*
+        n: num of nodes
+        h: tree height -- balanced log n, unbalanced n
+        time: O(n)
+        space: O(h)
+    */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return null;
+        if(root == p || root == q) return root;
+        
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        if(left != null && right != null) return root;
+        if(left != null) return left;
+        return right;
+    }
+}

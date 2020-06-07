47a1a1864f075c1f04a69e81414591595ec7598b
Hilal Tarakci <hlltarakci@gmail.com>
2020-06-07 19:25:18 +0300

    Create verifyingAnAlienDictionary.java

---
+++ code/f/extra/verifyingAnAlienDictionary.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/verifying-an-alien-dictionary/
+
+class Solution {
+    /*
+        c: total content of words alltogether
+        time: O(c)
+        space: O(1)
+    */
+    public boolean isAlienSorted(String[] words, String order) {
+        for(int i=1; i<words.length; i++) {
+            if(!before(words[i-1], words[i], order)) return false;
+        }
+        
+        return true;
+    }
+    
+    private boolean before(String prevWord, String currWord, String order) {
+        if(prevWord.length() > currWord.length() && prevWord.startsWith(currWord)) return false;
+        
+        int len = Math.min(prevWord.length(), currWord.length());
+        for(int i=0; i<len; i++) {
+            int prevCh = order.indexOf(prevWord.charAt(i));
+            int currCh = order.indexOf(currWord.charAt(i));
+            
+            if(prevCh > currCh) return false;
+            else if(prevCh < currCh) return true;
+        }
+        
+        return true;
+    }
+}

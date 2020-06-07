d1f1d9a870fb0d3f91226faa66cb07f040048237
Hilal Tarakci <hlltarakci@gmail.com>
2020-06-07 19:39:42 +0300

    Create findAllAnagramsInAString.java

---
+++ code/f/extra/findAllAnagramsInAString.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/find-all-anagrams-in-a-string/
+
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> list = new ArrayList<>();
+        if(p.length() > s.length()) return list;
+        
+        int[] refMap = new int[26];
+        for(char c: p.toCharArray()) refMap[c-'a']++;
+        
+        int[] map = new int[26];
+        for(int i=0; i<p.length(); i++) map[s.charAt(i)-'a']++;
+        if(Arrays.equals(refMap, map)) list.add(0);
+        
+        for(int i=p.length(); i<s.length(); i++) {
+            map[s.charAt(i-p.length())-'a']--;
+            map[s.charAt(i)-'a']++;
+            if(Arrays.equals(refMap, map)) list.add(i-p.length()+1);
+        }
+        
+        return list;
+    }
+}

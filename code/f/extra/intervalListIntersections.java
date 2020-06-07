c5828d54e267d672dfe196d593ff1277487d9971
Hilal Tarakci <hlltarakci@gmail.com>
2020-06-07 19:37:16 +0300

    Create intervalListIntersections.java

---
+++ code/f/extra/intervalListIntersections.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/interval-list-intersections/
+
+class Solution {
+    /*
+        time: O(a+b)
+        space O(a+b)
+    */
+    public int[][] intervalIntersection(int[][] A, int[][] B) {
+        List<int[]> list = new ArrayList<>();
+        
+        int indexA=0, indexB=0;
+        while(indexA < A.length && indexB < B.length) {
+            int start = Math.max(A[indexA][0], B[indexB][0]);
+            int end = Math.min(A[indexA][1], B[indexB][1]);
+            
+            if(start <= end) list.add(new int[] {start, end});
+            
+            if(A[indexA][1] < B[indexB][1]) indexA++;
+            else indexB++;
+        }
+        
+        
+        return list.toArray(new int[list.size()][]);
+    }
+}

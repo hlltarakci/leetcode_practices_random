7b5288e4b77857fc2a928ca68f11c957a25bf01a
Hilal Tarakci <hlltarakci@gmail.com>
2020-06-07 14:16:51 +0300

    Create continuousSubarraySum.java

---
+++ code/f/continuousSubarraySum.java
@@ -0,0 +1,45 @@
+// https://leetcode.com/explore/interview/card/facebook/55/dynamic-programming-3/3038/
+
+class Solution {
+    /*
+        time: O(n)
+        space: O(min(n, k))
+    */
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        
+        for(int i=0; i<nums.length; i++) {
+            sum += nums[i];
+            if(k != 0) sum = sum % k;
+            if(map.containsKey(sum)) {
+               if(i - map.get(sum) > 1) return true;
+            } else map.put(sum, i);
+        }
+        
+        return false;
+    }
+    
+    /* approach: dp
+    //    n: nums arr len
+    //    time: O(n^2)
+    //    space: O(n)
+    public boolean checkSubarraySum(int[] nums, int k) {
+        if(nums.length == 0) return false;
+        
+        int[] dp = new int[nums.length];
+        for(int i=1; i<nums.length; i++) dp[i] = dp[i-1] + nums[i];
+        
+        for(int i=0; i<nums.length; i++) {
+            for(int j=i+1; j<nums.length; j++) {
+                int sum = dp[j] - dp[i] + nums[i];
+                if( sum == k ||
+                    k != 0 && sum % k == 0) return true;
+            }
+        }
+        
+        return false;
+    }
+    */
+}

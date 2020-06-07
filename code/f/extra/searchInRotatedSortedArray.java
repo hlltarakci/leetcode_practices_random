61f38811603e7439325b466ab57a71232ad68f02
Hilal Tarakci <hlltarakci@gmail.com>
2020-06-07 19:33:46 +0300

    Create searchInRotatedSortedArray.java

---
+++ code/f/extra/searchInRotatedSortedArray.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+class Solution {
+    /*
+        n: arr length
+        time: O(log n)
+        space: O(1)
+    */
+    public int search(int[] nums, int target) {
+        if(nums == null) return -1;
+        int left = 0, right = nums.length-1;
+        while(left <= right) {
+            int mid = left + (right-left)/2;
+            
+            if(nums[mid] == target) return mid;
+            
+            if(nums[right] < nums[mid]) {
+                if(nums[left] <= target && target < nums[mid]) right = mid-1; 
+                else left = mid+1;
+            }
+            else{
+                if(nums[right] >= target && target > nums[mid])  left = mid+1; 
+                else right = mid-1; 
+            }
+            
+        }
+        
+        return -1;
+    }
+}
